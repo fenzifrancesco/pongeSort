aa9e81d16ac11e3942cacdb108f7cb18db00af1f
fenzi.francesco <fenzi.francesco@TAVE-PC11.OSDBSESTO.local>
2021-04-29 12:19:36 +0200

    add array

--- src/pongesort/PongeSort.java
+++ src/pongesort/PongeSort.java
@@ -15,7 +15,8 @@ public class PongeSort {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    char [] arr = {'a', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'r', 't'};
+        
     }
     
 }
